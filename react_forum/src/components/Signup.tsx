8340dae5df0e7b15134622fec5424e2c88064835
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-01-08 15:16:17 +0100

    add signup component

---
+++ react_forum/src/components/Signup.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async () => {
+    try {
+      let res = await fetch("http://localhost/master_project/php_rest_forum/api/user/signup.php", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+          confirm: confirm
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setName("");
+        setEmail("");
+        setMessage(resJson.message);
+      } else {
+        setMessage(resJson.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          value={name}
+          placeholder="Username"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="confirm"
+          value={confirm}
+          placeholder="Confirm"
+          onChange={(e) => setConfirm(e.target.value)}
+        />
+
+        <button type="submit">Create</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
