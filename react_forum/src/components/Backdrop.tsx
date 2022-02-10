96879e4739eb1f2dad71602912e28f08518bd924
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-02-10 11:32:21 +0100

    create modal components

---
+++ react_forum/src/components/Backdrop.tsx
@@ -0,0 +1,22 @@
+import {motion} from "framer-motion";
+
+type Props = {
+  children: JSX.Element;
+  onClick: () => void;
+};
+
+function Backdrop({children, onClick}: Props) {
+  return (
+    <motion.div
+      className="backdrop"
+      onClick={onClick}
+      initial={{opacity: 0}}
+      animate={{opacity: 1}}
+      exit={{opacity: 0}}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default Backdrop;
