669ab1b9ffa549ebcd8780ef13e43d0cdb628f6d
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-02-17 11:13:10 +0100

    format

--- react_forum/src/components/ModalChildren.tsx
+++ react_forum/src/components/ModalChildren.tsx
@@ -1,8 +1,10 @@
 import Login from "./Login";
 import Signup from "./Signup";
+
 type Props = {
     content: string;
   };
+
 function ModalChildren({content}: Props) {
     if (content == "Signup") return <Signup></Signup>;
     if (content == "Login") return <Login></Login>;
