a6124a6f340bbe70d524b3b942ab7e8294cd3387
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-02-17 19:40:16 +0100

    create login component

---
+++ react_forum/src/components/Login.tsx
@@ -0,0 +1,82 @@
+import { setegid } from "process";
+import { useRef, useState, useEffect, useContext } from "react"
+import axios from "../api/axios";
+import AuthContext from "../context/AuthProvider";
+
+const LOGIN_URL = "/user/login.php";
+
+function Login() {
+    const { setAuth } = useContext(AuthContext);
+
+    const emailRef = useRef<HTMLInputElement>(null);
+    const errRef = useRef<HTMLParagraphElement>(null);
+
+    const [email, setEmail] = useState("");
+
+    const [password, setPassword] = useState("");
+    const [errMsg, setErrMsg] = useState("");
+
+    useEffect(() => {
+        emailRef.current?.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [email, password])
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({ email, password }),
+                {
+                    headers: { "Content-Type": "application/json" },
+                    withCredentials: true
+                }
+            );
+            console.log(JSON.stringify(response?.data));
+            const accessToken = response?.data?.accessToken;
+            const role = response?.data?.role;
+            setAuth({ email, password, role, accessToken });
+            setEmail("");
+            setPassword("");
+        } catch (err: any) {
+            if (!err?.response) setErrMsg("No Server Response");
+            else if (err?.response?.status === 400) setErrMsg("Wrong Email or Password");
+            else if (err?.response?.status === 401) setErrMsg("Unauthorized");
+            else setErrMsg("Login Failed");
+            errRef.current?.focus();
+        }
+    }
+
+    return (
+        <>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    name="email"
+                    ref={emailRef}
+                    autoComplete="off"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required
+                    className="form-control"
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required
+                    className="form-control"
+                />
+                <button disabled={!email || !password} className="btn hollow">Login</button>
+            </form>
+        </>
+    );
+}
+export default Login;
