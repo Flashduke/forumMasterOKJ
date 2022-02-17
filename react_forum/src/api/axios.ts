26d2bd17a03295da891f282e9fe7e43d9d9317cc
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-02-17 14:55:07 +0100

    fix baseURL

--- react_forum/src/api/axios.ts
+++ react_forum/src/api/axios.ts
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-export default axios.create({baseURL: "http://localhost/php_rest_forum/api"});
+export default axios.create({baseURL: "http://localhost/forumMasterOKJ/php_rest_forum/api"});
