07c965fde09ff227c2a2e4a3b357a3539d10d745
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-02-21 20:21:44 +0100

    simplify auth provider context

--- react_forum/src/context/AuthProvider.tsx
+++ react_forum/src/context/AuthProvider.tsx
@@ -1,34 +1,12 @@
-import React, {
-  Dispatch,
-  useState,
-  SetStateAction,
-  Context,
-  createContext,
-} from 'react';
+import { useState, createContext } from 'react';
 
-type Children = {
-  children: React.ReactElement;
-};
+const AuthContext = createContext({
+  auth: null,
+  setAuth: (auth) => {},
+});
 
-interface AuthContextProps {
-  email: String | null;
-  password: String | null;
-  role: String | null;
-  accessToken: String | null;
-}
-
-const AuthContext: Context<{
-  auth: AuthContextProps;
-  setAuth: Dispatch<SetStateAction<AuthContextProps>>;
-}> = createContext(null);
-
-export function AuthProvider({ children }: Children) {
-  const [auth, setAuth] = useState({
-    email: null,
-    password: null,
-    role: null,
-    accessToken: null,
-  });
+export function AuthProvider({ children }) {
+  const [auth, setAuth] = useState({});
 
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
