e850abf606bebe6afb79b7c17ab6818b12500db2
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-02-27 17:31:53 +0100

    add feed, temp link to admin

--- react_forum/src/components/Home.tsx
+++ react_forum/src/components/Home.tsx
@@ -1,3 +1,15 @@
+import { Link } from 'react-router-dom';
+import Feed from './Feed';
+
 export default function Home() {
-  return <div className="content">Home</div>;
+  
+
+  return (
+    <div className="content">
+      Home
+      <br />
+      <Link to="/admin">Admin Page</Link>
+      <Feed></Feed>
+    </div>
+  );
 }
