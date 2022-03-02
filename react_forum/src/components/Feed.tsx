d44c309a90d3ae29960e2ace5b5f65b914ed0c1a
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-03-02 21:10:16 +0100

    add community feed support

--- react_forum/src/components/Feed.tsx
+++ react_forum/src/components/Feed.tsx
@@ -1,24 +1,33 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from '../api/axios';
 import { defaultPostData, postData } from '../models/Post';
 import Post from './Post';
 
-type Props = {};
+type Props = {
+  community?: string;
+  onCommunityPage?: boolean;
+};
 
-function Feed({}: Props) {
+function Feed({ community, onCommunityPage }: Props) {
+const navigate = useNavigate();
+
   const [allPosts, setAllPosts] = useState<postData>(defaultPostData);
 
   const getPosts = async () => {
     try {
-      const response = await axios.get('/post/read.php', {
-        withCredentials: true,
-      });
+      const response = await axios.get(
+        `/post/read.php${community ? '?community=' + community : ''}`,
+        {
+          withCredentials: true,
+        }
+      );
       const responseObj = response?.data;
       setAllPosts({ isLoaded: true, posts: responseObj?.posts });
       console.log(responseObj);
     } catch (err) {
       console.error(err);
+      navigate('/404');
     }
   };
 
@@ -31,7 +40,9 @@ function Feed({}: Props) {
       {!allPosts.isLoaded ? (
         <p>Loading...</p>
       ) : (
-        allPosts.posts.map((post, index) => <Post key={index} post={post}></Post>)
+        allPosts.posts.map((post, index) => (
+          <Post key={index} post={post} onCommunityPage={onCommunityPage}></Post>
+        ))
       )}
     </section>
   );
