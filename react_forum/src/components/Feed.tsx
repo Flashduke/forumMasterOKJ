fadeb117ec5673d7f812f530b9da1e2161bb14ab
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-03-06 18:25:48 +0100

    add rating request

--- react_forum/src/components/Feed.tsx
+++ react_forum/src/components/Feed.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from '../api/axios';
+import useAuth from '../hooks/useAuth';
+import useRating from '../hooks/useRating';
 import { defaultPostData, postData } from '../models/Post';
 import Post from './Post';
 
@@ -12,6 +14,8 @@ type Props = {
 };
 
 function Feed({ community, onCommunityPage, profile, onProfilePage }: Props) {
+  const { auth } = useAuth();
+  const { rating, setRated } = useRating();
   const navigate = useNavigate();
 
   const [allPosts, setAllPosts] = useState<postData>(defaultPostData);
@@ -20,24 +24,52 @@ function Feed({ community, onCommunityPage, profile, onProfilePage }: Props) {
     try {
       const response = await axios.get(
         `/post/read.php${
-          community ? '?community=' + community : profile ? '?profile=' + profile : ''
+          community
+            ? '?community=' + community
+            : profile
+            ? '?profile=' + profile
+            : ''
         }`,
-        {
-          withCredentials: true,
-        }
+        { withCredentials: true }
       );
       const responseObj = response?.data;
       setAllPosts({ isLoaded: true, posts: responseObj?.posts });
-      console.log(responseObj);
     } catch (err) {
       console.error(err);
       navigate('/404');
     }
   };
 
+  const getRated = async () => {
+    try {
+      const response = await axios.get(
+        'rated_content/read.php?profile=' + auth?.name,
+        { withCredentials: true }
+      );
+      const likedComments = response?.data?.liked_comments;
+      const dislikedComments = response?.data?.disliked_comments;
+      const likedPosts = response?.data?.liked_posts;
+      const dislikedPosts = response?.data?.disliked_posts;
+      setRated({
+        likedComments,
+        dislikedComments,
+        likedPosts,
+        dislikedPosts,
+      });
+      console.log(rating);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
     getPosts();
   }, []);
+
+  useEffect(() => {
+    getRated();
+    getPosts();
+  }, [auth]);
 
   return (
     <section role="feed" className="feed">
@@ -50,7 +82,7 @@ function Feed({ community, onCommunityPage, profile, onProfilePage }: Props) {
             post={post}
             onCommunityPage={onCommunityPage}
             onProfilePage={onProfilePage}
-          ></Post>
+          />
         ))
       )}
     </section>
