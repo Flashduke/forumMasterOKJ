da5db4b27522f9b38add93b3be4b144c8efa1f68
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-03-10 18:50:33 +0100

    add single post support

--- react_forum/src/models/Post.ts
+++ react_forum/src/models/Post.ts
@@ -1,6 +1,6 @@
 export interface IPost {
-  author: string;
-  communityName: string;
+  author?: string;
+  communityName?: string;
   commentCount: number;
   content: string;
   createdAt: Date;
@@ -14,8 +14,23 @@ export interface postData {
   isLoaded: boolean;
   posts: IPost[];
 }
-
+export interface singlePostData {
+  isLoaded: boolean;
+  post: IPost;
+}
 export const defaultPostData: postData = {
   isLoaded: false,
   posts: [],
+};
+export const defaultSinglePostData: singlePostData = {
+  isLoaded: false,
+  post: {
+    commentCount: 0,
+    content: '',
+    createdAt: null,
+    thumbsDowns: 0,
+    thumbsUps: 0,
+    id: '',
+    title: '',
+  },
 };
