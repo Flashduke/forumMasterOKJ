66c620e4b763a38d57600690a4b2a7f1db931777
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-03-11 18:57:39 +0100

    create helper functions file

---
+++ react_forum/src/helpers.ts
@@ -0,0 +1,14 @@
+export const formatAuthor = (author: string, username: string): string => {
+  if (author === username) return 'You';
+  return author;
+};
+
+export const formatCount = (count: number): string => {
+  if (count / 1000000000 > 1)
+    return (Math.round((count / 1000000000) * 100) / 100).toString() + 'B';
+  if (count / 1000000 > 1)
+    return (Math.round((count / 1000000) * 100) / 100).toString() + 'M';
+  if (count / 2000 > 1)
+    return (Math.round((count / 1000) * 100) / 100).toString() + 'K';
+  return count.toString();
+};
