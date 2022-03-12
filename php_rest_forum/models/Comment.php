3e2a85b4dd9f6f0ecc7009fb1d04e272cee8e26c
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-03-12 18:06:39 +0100

    add comments_view

--- php_rest_forum/models/Comment.php
+++ php_rest_forum/models/Comment.php
@@ -1,205 +1,140 @@
 <?php
-    class Comment {
-        private $conn;
-        private $table = 'comments';
+class Comment
+{
+    private $conn;
+    private $table = 'comments';
+    private $view = 'comments_view';
 
-        //comment props
-        public $id;
-        public $createdAt;
-        public $content;
-        public $postID;
-        public $postTitle;
-        public $userID;
-        public $userName;
-        public $thumbsDowns;
-        public $thumbsUps;
+    //comment props
+    public $id;
+    public $createdAt;
+    public $communityName;
+    public $content;
+    public $condition;
+    public $postID;
+    public $postTitle;
+    public $userID;
+    public $userName;
+    public $thumbsDowns;
+    public $thumbsUps;
 
-        //ctor
-        public function __construct($db)
-        {
-            $this->conn = $db;
-        }
+    //ctor
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
 
-        //get communities
-        public function read() {
-            //create query
-            $query = 'SELECT
-                p.title AS postTitle,
-                u.name AS userName,
-                c.id,
-                c.createdAt,
-                c.content,
-                c.postID,
-                c.userID,
-                c.thumbsDowns,
-                c.thumbsUps
-            FROM
-                ' . $this->table . ' c
-            LEFT JOIN
-                users u ON c.userID = u.id
-            LEFT JOIN
-                posts p ON c.postID = p.id
-            ORDER BY
-                c.createdAt DESC';
+    //get communities
+    public function read()
+    {
+        //create query
+        $query = 'SELECT * FROM ' . $this->view . ' WHERE ' . $this->condition . ' = :value';
 
-            //prepare statement
-            $stmt = $this->conn->prepare($query);
+        //prepare statement
+        $stmt = $this->conn->prepare($query);
 
-            //execute statement
-            $stmt->execute();
+        if ($this->condition == 'postID') $stmt->bindParam(':value', $this->postID);
+        if ($this->condition == 'author') $stmt->bindParam(':value', $this->userName);
 
-            return $stmt;
-        }
+        //execute statement
+        $stmt->execute();
 
-        //get single comment
-        public function readSingle() {
-            //create query
-            $query = 'SELECT
-                p.title AS postTitle,
-                u.name AS userName,
-                c.id,
-                c.createdAt,
-                c.content,
-                c.postID,
-                c.userID,
-                c.thumbsDowns,
-                c.thumbsUps
-            FROM
-                ' . $this->table . ' c
-            LEFT JOIN
-                users u ON c.userID = u.id
-            LEFT JOIN
-                posts p ON c.postID = p.id
-            WHERE
-                c.id = ?
-            LIMIT 0,1';
+        return $stmt;
+    }
 
-            //prepare statement
-            $stmt = $this->conn->prepare($query);
-
-            //bind ID
-            $stmt->bindParam(1, $this->id);
-
-            //execute statement
-            $stmt->execute();
-
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            //set properties
-            $this->createdAt = $row['createdAt'];
-            $this->content = $row['content'];
-            $this->postID = $row['postID'];
-            $this->postTitle = $row['postTitle'];
-            $this->userID = $row['userID'];
-            $this->userName = $row['userName'];
-            $this->thumbsDowns = $row['thumbsDowns'];
-            $this->thumbsUps = $row['thumbsUps'];
-        }
-
-        //create comment
-        public function create() {
-            //create query
-            $query = 'INSERT INTO ' . $this->table . '
+    //create comment
+    public function create()
+    {
+        //create query
+        $query = 'INSERT INTO ' . $this->table . '
             SET
                 content = :content,
                 postID = :postID,
-                userID = :userID,
-                thumbsDowns = :thumbsDowns,
-                thumbsUps = :thumbsUps';
+                userID = :userID';
 
-            //prepare statement
-            $stmt = $this->conn->prepare($query);
+        //prepare statement
+        $stmt = $this->conn->prepare($query);
 
-            //clean data
-            $this->content = htmlspecialchars(strip_tags($this->content));
-            $this->postID = htmlspecialchars(strip_tags($this->postID));
-            $this->userID = htmlspecialchars(strip_tags($this->userID));
-            $this->thumbsDowns = htmlspecialchars(strip_tags($this->thumbsDowns));
-            $this->thumbsUps = htmlspecialchars(strip_tags($this->thumbsUps));
+        //clean data
+        $this->content = htmlspecialchars(strip_tags($this->content));
+        $this->postID = htmlspecialchars(strip_tags($this->postID));
+        $this->userID = htmlspecialchars(strip_tags($this->userID));
 
-            //bind data
-            $stmt->bindParam(':content', $this->content);
-            $stmt->bindParam(':postID', $this->postID);
-            $stmt->bindParam(':userID', $this->userID);
-            $stmt->bindParam(':thumbsDowns', $this->thumbsDowns);
-            $stmt->bindParam(':thumbsUps', $this->thumbsUps);
+        //bind data
+        $stmt->bindParam(':content', $this->content);
+        $stmt->bindParam(':postID', $this->postID);
+        $stmt->bindParam(':userID', $this->userID);
 
-            //execute query
-            if ($stmt->execute()) {
-                return true;
-            }
-
-            //print error if something goes wrong
-            printf("Error: %s.\n", $stmt->error);
-
-            return false;
+        //execute query
+        if ($stmt->execute()) {
+            return true;
         }
 
-        //update comment
-        public function update() {
-            //create query
-            $query = 'UPDATE ' . $this->table . '
+        //print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+    }
+
+    //update comment
+    public function update()
+    {
+        //create query
+        $query = 'UPDATE ' . $this->table . '
             SET
                 content = :content,
-                postID = :postID,
-                userID = :userID,
-                thumbsDowns = :thumbsDowns,
-                thumbsUps = :thumbsUps
+                userID = :userID
             WHERE
                 id = :id';
 
-            //prepare statement
-            $stmt = $this->conn->prepare($query);
+        //prepare statement
+        $stmt = $this->conn->prepare($query);
 
-            //clean data
-            $this->id = htmlspecialchars(strip_tags($this->id));
-            $this->content = htmlspecialchars(strip_tags($this->content));
-            $this->postID = htmlspecialchars(strip_tags($this->postID));
-            $this->userID = htmlspecialchars(strip_tags($this->userID));
-            $this->thumbsDowns = htmlspecialchars(strip_tags($this->thumbsDowns));
-            $this->thumbsUps = htmlspecialchars(strip_tags($this->thumbsUps));
+        //clean data
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->content = htmlspecialchars(strip_tags($this->content));
+        $this->userID = htmlspecialchars(strip_tags($this->userID));
 
-            //bind data
-            $stmt->bindParam(':id', $this->id);
-            $stmt->bindParam(':content', $this->content);
-            $stmt->bindParam(':postID', $this->postID);
-            $stmt->bindParam(':userID', $this->userID);
-            $stmt->bindParam(':thumbsDowns', $this->thumbsDowns);
-            $stmt->bindParam(':thumbsUps', $this->thumbsUps);
-
-            //execute query
-            if ($stmt->execute()) {
-                return true;
-            }
-
-            //print error if something goes wrong
-            printf("Error: %s.\n", $stmt->error);
-
-            return false;
+        //bind data
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':content', $this->content);
+        $stmt->bindParam(':userID', $this->userID);
+        //execute query
+        if ($stmt->execute()) {
+            return true;
         }
 
-        //delete comment
-        public function delete() {
-            //create query
-            $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id';
+        //print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
 
-            //prepare statement
-            $stmt = $this->conn->prepare($query);
-
-            //clean data
-            $this->id = htmlspecialchars(strip_tags($this->id));
-
-            //bind data
-            $stmt->bindParam(':id', $this->id);
-
-            //execute query
-            if ($stmt->execute()) {
-                return true;
-            }
-
-            //print error if something goes wrong
-            printf("Error: %s.\n", $stmt->error);
-
-            return false;
-        }
+        return false;
     }
+
+    //delete comment
+    public function delete()
+    {
+        //create query
+        $query = 'DELETE FROM ' . $this->table . ' WHERE id = :id AND userID = :userID';
+
+        //prepare statement
+        $stmt = $this->conn->prepare($query);
+
+        //clean data
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->id = htmlspecialchars(strip_tags($this->userID));
+
+        //bind data
+        $stmt->bindParam(':id', $this->id);
+        $stmt->bindParam(':userID', $this->userID);
+
+        //execute query
+        if ($stmt->execute()) {
+            return true;
+        }
+
+        //print error if something goes wrong
+        printf("Error: %s.\n", $stmt->error);
+
+        return false;
+    }
+}
