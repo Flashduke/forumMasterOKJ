7d88a3fe29edea8a22413d1c6f5872d2e3e52693
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-03-17 18:17:26 +0100

    adjust layout for wrapped pages

--- react_forum/src/components/Layout.tsx
+++ react_forum/src/components/Layout.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import { RatingProvider } from '../context/RatingProvider';
 import Navbar from './Navbar';
 
 function Layout() {
   return (
     <>
       <Navbar />
-      <div className="content">
         <Outlet />
-      </div>
     </>
   );
 }
