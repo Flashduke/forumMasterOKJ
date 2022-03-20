27297799b45a27023a302261c660be7c4d0b6762
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-03-20 19:23:45 +0100

    add export to base url

--- react_forum/src/api/axios.ts
+++ react_forum/src/api/axios.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost/forumMasterOKJ/php_rest_forum/api';
+export const BASE_URL = 'http://localhost/forumMasterOKJ/php_rest_forum/';
 
-export default axios.create({ baseURL: BASE_URL });
+export default axios.create({ baseURL: BASE_URL + 'api' });
 
 export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
+  baseURL: BASE_URL + 'api',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
