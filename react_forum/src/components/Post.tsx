f16bde15c46a98533d2deeb1d2510ce442b88a1a
Flashduke <35380324+Flashduke@users.noreply.github.com>
2022-04-14 13:54:42 +0200

    add delete button

--- react_forum/src/components/Post.tsx
+++ react_forum/src/components/Post.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Link, useNavigate } from 'react-router-dom';
 import remarkGfm from 'remark-gfm';
 import TimeAgo from 'timeago-react';
 import { formatAuthor, formatCount } from '../helpers';
 import useAuth from '../hooks/useAuth';
+import useAxiosPrivate from '../hooks/useAxiosPrivate';
 import { IPost } from '../models/Post';
 import CommentSection from './CommentSection';
 import CreateContent from './CreateContent';
@@ -18,16 +20,32 @@ type Props = {
 
 function Post({ post, onCommunityPage, onProfilePage, onPostPage }: Props) {
   const { auth } = useAuth();
+  const axiosPrivate = useAxiosPrivate();
+  const controller = new AbortController();
 
   const navigate = useNavigate();
+
+  const [deleted, setDeleted] = useState(false);
 
   const handleChildElementClick = (e: React.MouseEvent<HTMLElement>) => {
     e.stopPropagation();
   };
 
+  const handleDelete = async () => {
+    try {
+      const response = await axiosPrivate.delete(`/${'post'}/delete.php`, {
+        signal: controller.signal,
+        data: JSON.stringify({ id: post.id }),
+      });
+      if (response.status === 200) setDeleted(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <article
-      className={`post ${!onPostPage && 'post-preview'}`}
+      className={`post ${!onPostPage && 'post-preview'} ${deleted && 'd-none'}`}
       aria-labelledby={'post_' + post.id}
       onClick={() =>
         !onPostPage &&
@@ -115,15 +133,38 @@ function Post({ post, onCommunityPage, onProfilePage, onPostPage }: Props) {
 
         <p aria-label="Comment count">{formatCount(post.commentCount)}</p>
       </div>
-      {onPostPage && <CreateContent type='comment' postID={post.id}></CreateContent>}
+      {onPostPage && (
+        <CreateContent type="comment" postID={post.id}></CreateContent>
+      )}
       {onPostPage &&
         (post.commentCount != 0 ? (
           <CommentSection postID={post.id} />
-        ) : (<>
-          
-          <p>It's quiet here...</p>
-        </>
+        ) : (
+          <>
+            <p>It's quiet here...</p>
+          </>
         ))}
+      {(auth.name === post.author ||
+        auth.role === 'owner' ||
+        auth.role === 'admin') && (
+        <div
+          className="delete-container"
+          onClick={(e) => handleChildElementClick(e)}
+        >
+          <button className="btn-delete" onClick={handleDelete}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.2rem"
+              height="1.2rem"
+              fill="currentColor"
+              className="bi bi-trash-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+            </svg>
+          </button>
+        </div>
+      )}
     </article>
   );
 }
